fd94d39d5143115fd091d1f1041ef1e0b00a62f8
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2026-04-22 21:42:47 +0300

    hw6 fix provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Providers from "./providers";
+import Providers from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
